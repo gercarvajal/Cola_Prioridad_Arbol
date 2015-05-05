d7d06de260ab4280a473171cb7d85539adce8fc6
Sliferxx <kaioken20_70@live.com>
2015-05-04 20:10:03 -0400

    Cola_AB.h
    HEADER DE COLA

---
+++ Cola_AB.h
@@ -0,0 +1,17 @@
+#ifndef STDIO_H
+#include <stdio.h>
+#define STDIO_H
+#endif 
+
+#ifndef STDLIB_H
+#include <stdlib.h>
+#define STDLIB_H
+#endif
+
+
+#define Tam_Arreglo 15 
+//a continucion se crea la estructura cola Arbol//
+
+typedef struct Nodo_Cola_AB {
+int unDato,tam,pto_insercion;
+}Cola_AB;
